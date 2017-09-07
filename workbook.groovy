bb565932d265c859e0cff6e4f00dd29113215da2
Ola Ajibode <searchs@users.noreply.github.com>
2017-09-07 11:08:52 +0100

    Sorting Algo added

--- workbook.groovy
+++ workbook.groovy
@@ -60,3 +60,55 @@ def qs = [limitTo: '[nerdy]', firstName:'Guillaume', lastName: 'Laforge'].collec
 String jsonTxt = "$base$qs".toURL().text
 def json = new JsonSlurper().parseText(jsonTxt)
 println json.value.joke
+
+String name;
+
+String n = (name != null && name.size() > 0 ? name: 'World')
+n = name ?: 'World'
+
+println "Allo, $n"
+
+//Implementing Sorting Algo in Groovy
+List strings = 'this is a list of strings'.split()
+println strings
+
+import java.Collections.*
+
+//Java natural sort
+Collections.sort(strings)
+println strings
+
+// Java sort by length
+Collections.sort(strings, new Comparator<String>(){
+    int compare(String s1, String s2){
+        s1.size() <=> s2.size()
+    }
+})
+
+println strings
+assert strings*.size() == [1,2,2,4,4,7]
+
+
+//Groovy natural sort
+println strings.sort(false, new Comparator<String>(){
+    int compare(String s1, String s2){
+        s1.size() <=> s2.size()
+    }
+})
+
+
+//Groovy reverse length sort with a 2-param closure
+println strings.sort(false) {s1,s2 ->
+s2.size() <=> s1.size()
+}
+
+
+//Groovy length sort with a 1-para closure
+println "Single Params Closure"
+println strings.sort(false) {it.size()}
+
+//Groovy sort by length, then equal lenths alphabethically
+println strings.sort(false) {String s1, String s2 ->
+s1.size() <=> s2.size() ?: s2 <=> s1
+}
+
